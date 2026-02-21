e477f0914780e2c593ac646c8c26a3604af54adc
shailu666687-cyber <shailu666687@gmail.com>
2026-02-21 14:15:12 +0530

    Add initial configuration for ShopStore Reseller

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    APP_NAME: "ShopStore Reseller",
+    CURRENCY: "₹",
+    TELEGRAM_BOT_TOKEN: "7996956533:AAGRbrHJva3t0Pyfu7Met1ttg2w-974msb4", // Change this
+    TELEGRAM_CHAT_ID: "8450221415"      // Change this
+};
+
